ccac976e6182c7fcce843366034105134b035fb0
Rom-Freedom <romeo.dark@mail.ru>
2024-02-23 23:38:19 +0300

    create modules for each page

--- js/main.js
+++ js/main.js
@@ -3,38 +3,35 @@ import { getPageContainer } from './components/pageContainer.js'
 import { getMainTitle} from './components/mainTitle.js'
 import { getDesc } from './components/desc.js'
 
-//Pages
-import { getMainPage } from './pages/main.js'
-import { getProductPage } from './pages/product.js'
-import { getCatalogPage } from './pages/catalog.js'
-import { getBasketPage } from './pages/basket.js'
-
 const app = document.getElementById('app')
-
-
 
 const header = getHeader()
 const pageContainer = getPageContainer()
 
 //Navigation
-export function navigation(page) {
+export async function navigation(page) {
     pageContainer.innerHTML = ''
     switch (page) {
       case 'catalog':
-        const catalogPage = getCatalogPage() 
+        const ModuleCatalog = await import('./pages/catalog.js')
+        const catalogPage = ModuleCatalog.getCatalogPage() 
         pageContainer.append(catalogPage)
         break
       case 'basket':
-        const BasketPage = getBasketPage() 
+        const ModuleBasket = await import('./pages/basket.js')
+        const BasketPage = ModuleBasket.getBasketPage() 
         pageContainer.append(BasketPage)
         break
       default:
-        const mainPage = getMainPage()
+        const ModuleMain = await import('./pages/main.js')
+        const mainPage = ModuleMain.getMainPage()
         pageContainer.append(mainPage)
         break
     }
 }
 
 navigation()
+
+const router = new Navigo('/');
 
 app.append(header, pageContainer)
